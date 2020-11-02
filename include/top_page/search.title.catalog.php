ece112889e2879c92d3cdb7e28cfbdf63452ddce
TimVance <2DngxvshA6GUGcQ>
2020-11-02 13:25:38 +0300

    Установил старый поиск

--- include/top_page/search.title.catalog.php
+++ include/top_page/search.title.catalog.php
@@ -1,49 +1,55 @@
 <?$APPLICATION->IncludeComponent(
-	"arturgolubev:search.title", 
-	"search_cat", 
-	array(
-		"CATEGORY_0" => array(
-			0 => "iblock_aspro_optimus_catalog",
-		),
-		"CATEGORY_0_TITLE" => "ALL",
-		"CATEGORY_0_iblock_aspro_max_catalog" => array(
-			0 => "54",
-			1 => "49",
-		),
-		"CATEGORY_0_iblock_aspro_optimus_catalog" => array(
-			0 => "14",
-			1 => "26",
-		),
-		"CATEGORY_OTHERS_TITLE" => "OTHER",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => "search_cat",
-		"COMPOSITE_FRAME_MODE" => "A",
-		"COMPOSITE_FRAME_TYPE" => "AUTO",
-		"CONTAINER_ID" => "title-search_fixed",
-		"CONVERT_CURRENCY" => "N",
-		"FILTER_NAME" => "",
-		"INPUT_ID" => "title-search-input_fixed",
-		"NUM_CATEGORIES" => "1",
-		"ORDER" => "date",
-		"PAGE" => CMax::GetFrontParametrValue("CATALOG_PAGE_URL"),
-		"PREVIEW_HEIGHT" => "38",
-		"PREVIEW_TRUNCATE_LEN" => "50",
-		"PREVIEW_WIDTH" => "38",
-		"PRICE_CODE" => array(
-		),
-		"PRICE_VAT_INCLUDE" => "Y",
-		"SEARCH_ICON" => $arParams["SEARCH_ICON"]=="Y"?"Y":"N",
-		"SHOW_ANOUNCE" => "N",
-		"SHOW_INPUT" => "Y",
-		"SHOW_INPUT_FIXED" => "Y",
-		"SHOW_OTHERS" => "N",
-		"SHOW_PREVIEW" => "Y",
-		"TOP_COUNT" => "10",
-		"USE_LANGUAGE_GUESS" => "Y",
-		"SHOW_PREVIEW_TEXT" => "Y"
-	),
-	false,
-	array(
-		"ACTIVE_COMPONENT" => "Y"
-	)
+    "datainlife:iblocksearch",
+    ".default",
+    array(
+        "CATALOG_FOLDER" => "/catalog/",
+        "COMPOSITE_FRAME_MODE" => "A",
+        "COMPOSITE_FRAME_TYPE" => "AUTO",
+        "CONTAINER_ID" => "di-search",
+        "EXT_FILTER" => "shopFilter",
+        "INPUT_ID" => "di-search-input",
+        "INPUT_SECTION_ID" => "di-search-section-id-input",
+        "PRICE_CODE" => array(
+            0 => "Розничная цена",
+        ),
+        "PROPERTY_CODE" => array(
+        ),
+        "RANDOM_ELEMENTS_COUNT" => "4",
+        "RANDOM_ELEMENTS_PICTURE_FIELD" => "PREVIEW_PICTURE",
+        "RANDOM_ELEMENTS_PICTURE_HEIGHT" => "100",
+        "RANDOM_ELEMENTS_PICTURE_WIDTH" => "100",
+        "RESULT_CONTAINER_ID" => "di-search-result",
+        "SECTIONS_COUNT_TOP" => "5",
+        "SECTION_ID" => "",
+        "COMPONENT_TEMPLATE" => ".default"
+    ),
+    false
 );?>
+<style>
+    #di-search-input {
+        width: calc(100% - 90px);
+        height: 40px;
+    }
+    .iblock-search {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 100%;
+    }
+    .iblock-search > form {
+        width: 100%;
+    }
+    .iblock-search button:hover, .iblock-search button:focus {
+        background: #f77724;
+    }
+    .iblock-search button {
+        display: inline-block;
+        vertical-align: top;
+        border: 1px solid #ef6508;
+        background: #ef6508;
+        color: #fff;
+        padding: 6px 10px 5px;
+        height: 40px;
+        margin-right: 10px;
+    }
+</style>
